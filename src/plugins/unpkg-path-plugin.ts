1959f675211637f7145a416a62dacec666e0c6fb
Yazan <me@yazanalmatar.com>
2021-03-06 18:19:30 -0500

    replaced if statements with onResolve

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -11,27 +11,26 @@ export const unpkgPathPlugin = (inputCode: string) => {
   return {
     //for debugging purposes
     name: "unpkg-path-plugin",
+
     //build represents the bundling process
     setup(build: esbuild.PluginBuild) {
+      build.onResolve({ filter: /(^index\.js$)/ }, () => {
+        return { path: "index.js", namespace: "a" };
+      });
+
+      build.onResolve({ filter: /^\.+\// }, (args: any) => {
+        return {
+          namespace: "a",
+          path: new URL(args.path, "https://unpkg.com" + args.resolveDir + "/")
+            .href,
+        };
+      });
       //the plugin will overwrites esbuild natural behavior to make it possible to
       //run in the browswer because it was designed to run on a file system
       build.onResolve({ filter: /.*/ }, async (args: any) => {
-        console.log("onResole", args);
-        if (args.path == "index.js") {
-          return { path: args.path, namespace: "a" };
-        }
-
         //It handles the resolutoin of relative files
         //check if there were relative paths  and modify the URL to make it a valid URL
-        if (args.path.includes("./") || args.path.includes("../")) {
-          return {
-            namespace: "a",
-            path: new URL(
-              args.path,
-              "https://unpkg.com" + args.resolveDir + "/"
-            ).href,
-          };
-        }
+
         return {
           namespace: "a",
           path: `https://unpkg.com/${args.path}`,
