b806ad87116f1cbefd19539b6db5615d1c0c7773
Yazan <me@yazanalmatar.com>
2021-03-25 00:26:00 -0400

    added fragments

--- src/components/cell-list.tsx
+++ src/components/cell-list.tsx
@@ -1,6 +1,7 @@
 import { useTypedSelector } from "../hooks/useTypedSelector";
 import CellListItem from "./cell-list-item";
 import AddCell from "./add-cell";
+import { Fragment } from "react";
 const CellList: React.FC = () => {
   const cells = useTypedSelector(({ cells }) => {
     // @ts-ignore
@@ -11,15 +12,15 @@ const CellList: React.FC = () => {
   });
 
   const renderedCells = cells.map((cell) => (
-    <>
+    <Fragment key={cell.id}>
       <AddCell nextCellId={cell.id} />
-      <CellListItem cell={cell} key={cell.id} />
-    </>
+      <CellListItem cell={cell} />
+    </Fragment>
   ));
   return (
     <div>
       {renderedCells}
-      <AddCell nextCellId={null} />
+      <AddCell nextCellId={null} forceVisible={cells.length === 0} />
     </div>
   );
 };
