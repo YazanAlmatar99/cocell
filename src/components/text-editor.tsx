cd3459d20d5491ebcd746b63e386309c332e2e33
Yazan <me@yazanalmatar.com>
2021-03-24 23:25:16 -0400

    added initial value

--- src/components/text-editor.tsx
+++ src/components/text-editor.tsx
@@ -39,7 +39,7 @@ const TextEditor: React.FC<TextEditorProps> = ({ cell }) => {
   return (
     <div onClick={() => setEditing(true)} className="text-editor card">
       <div className="card-content">
-        <MDEditor.Markdown source={cell.content} />
+        <MDEditor.Markdown source={cell.content || "Click to edit"} />
       </div>
     </div>
   );
