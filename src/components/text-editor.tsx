d3399a1cdf49343ade95de9e88882cc248b03a43
Yazan <me@yazanalmatar.com>
2021-03-24 23:07:21 -0400

    added useActions functions instead of local state

--- src/components/text-editor.tsx
+++ src/components/text-editor.tsx
@@ -1,12 +1,15 @@
 import "./text-editor.css";
 import React, { useState, useEffect, useRef } from "react";
 import MDEditor from "@uiw/react-md-editor";
-
-const TextEditor: React.FC = () => {
+import { Cell } from "../state/cell";
+import { useActions } from "../hooks/use-actions";
+interface TextEditorProps {
+  cell: Cell;
+}
+const TextEditor: React.FC<TextEditorProps> = ({ cell }) => {
   const ref = useRef<HTMLDivElement | null>(null);
   const [editing, setEditing] = useState(false);
-  const [value, setValue] = useState("# Header");
-
+  const { updateCell } = useActions();
   useEffect(() => {
     const listener = (event: MouseEvent) => {
       if (
@@ -26,14 +29,17 @@ const TextEditor: React.FC = () => {
   if (editing) {
     return (
       <div ref={ref} className="text-editor">
-        <MDEditor value={value} onChange={(v) => setValue(v || "")} />
+        <MDEditor
+          value={cell.content}
+          onChange={(v) => updateCell(cell.id, v || "")}
+        />
       </div>
     );
   }
   return (
     <div onClick={() => setEditing(true)} className="text-editor card">
       <div className="card-content">
-        <MDEditor.Markdown source={value} />
+        <MDEditor.Markdown source={cell.content} />
       </div>
     </div>
   );
