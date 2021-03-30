13c4b4f95ee618c2fedb5560afe0d6e6e8778944
Yazan <me@yazanalmatar.com>
2021-03-29 20:34:49 -0400

    exported bundle reducer as default

--- src/state/reducers/bundlesReducer.ts
+++ src/state/reducers/bundlesReducer.ts
@@ -34,3 +34,5 @@ const reducer = produce(
     }
   }
 );
+
+export default reducer;
--- src/state/reducers/index.ts
+++ src/state/reducers/index.ts
@@ -1,6 +1,7 @@
 import { combineReducers } from "redux";
 import cellsReducer from "./cellsReducer";
 import bundlesReducer from "./bundlesReducer";
+
 const reducers = combineReducers({
   cells: cellsReducer,
   bundles: bundlesReducer,
