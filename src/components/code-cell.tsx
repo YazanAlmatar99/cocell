a5302fd015c6606e995e8a9ef2b775d47c407524
Yazan <me@yazanalmatar.com>
2021-03-31 20:26:37 -0400

    defined show function

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -19,7 +19,22 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
     const { data, order } = state.cells;
     //@ts-ignore
     const orderedCells = order.map((id) => data[id]);
-    const cumulativeCode = [];
+    const cumulativeCode = [
+      `
+        const show = (value) => {
+          const root = document.querySelector('#root')
+          if (typeof value == 'object') {
+            if (value.$$typeof && value.props) {
+              ReactDOM.render(value,root)
+            } else {
+              root.innerHTML = JSON.stringify(value);
+            }
+          } else {
+            root.innerHTML = value;
+          }
+        } 
+        `,
+    ];
     for (let c of orderedCells) {
       if (c.type === "code") {
         cumulativeCode.push(c.content);
