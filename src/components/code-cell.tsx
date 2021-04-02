79c9b32af47f47f3531f0ca7bd4e4e76ed48383d
Yazan <me@yazanalmatar.com>
2021-04-02 14:26:39 -0400

    extracted code into a hook

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -1,73 +1,37 @@
 import "./code-cell.css";
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
 import CodeEditor from "./code-editor";
 import Preview from "./preview";
 import Resizable from "./resizable";
 import { Cell } from "../state/cell";
 import { useActions } from "../hooks/use-actions";
 import { useTypedSelector } from "../hooks/useTypedSelector";
+import { useCumulativeCode } from "../hooks/use-cumulative-code";
+
 interface CodeCellProps {
   cell: Cell;
 }
 
 const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
   const { updateCell, createBundle } = useActions();
+  const bundle = useTypedSelector((state: any) => state.bundles[cell.id]);
+  const cumulativeCode = useCumulativeCode(cell.id);
 
-  const bundle = useTypedSelector((state) => state.bundles[cell.id]);
-  const cumulativeCode = useTypedSelector((state) => {
-    //@ts-ignore
-    const { data, order } = state.cells;
-    //@ts-ignore
-    const orderedCells = order.map((id) => data[id]);
-
-    const showFunc = `
-    import _React from 'react';
-    import _ReactDOM from 'react-dom';
-    var show = (value) => {
-      const root = document.querySelector('#root')
-      if (typeof value == 'object') {
-        if (value.$$typeof && value.props) {
-          _ReactDOM.render(value,root)
-        } else {
-          root.innerHTML = JSON.stringify(value);
-        }
-      } else {
-        root.innerHTML = value;
-      }
-    } 
-    `;
-    const showFuncNoop = "var show = () => {}";
-    const cumulativeCode = [];
-    for (let c of orderedCells) {
-      if (c.type === "code") {
-        if (c.id === cell.id) {
-          cumulativeCode.push(showFunc);
-        }
-        cumulativeCode.push(c.content);
-      } else {
-        cumulativeCode.push(showFuncNoop);
-      }
-      if (c.id === cell.id) {
-        break;
-      }
-    }
-    return cumulativeCode;
-  });
   useEffect(() => {
     if (!bundle) {
-      createBundle(cell.id, cumulativeCode.join("\n"));
+      createBundle(cell.id, cumulativeCode);
       return;
     }
+
     const timer = setTimeout(async () => {
-      //to reset the iframe every time before execution
-      createBundle(cell.id, cumulativeCode.join("\n"));
-    }, 1200);
+      createBundle(cell.id, cumulativeCode);
+    }, 750);
 
     return () => {
       clearTimeout(timer);
     };
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [cumulativeCode.join("\n"), cell.id, createBundle]);
+  }, [cumulativeCode, cell.id, createBundle]);
 
   return (
     <Resizable direction="vertical">
