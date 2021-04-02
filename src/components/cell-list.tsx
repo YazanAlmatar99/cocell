6f45431ee07ab5d0a377f8925e28f01c0cd153ba
Yazan <me@yazanalmatar.com>
2021-04-02 17:43:00 -0400

    added margin on  sides

--- src/components/cell-list.tsx
+++ src/components/cell-list.tsx
@@ -1,3 +1,4 @@
+import "./cell-list.css";
 import { useTypedSelector } from "../hooks/useTypedSelector";
 import CellListItem from "./cell-list-item";
 import AddCell from "./add-cell";
@@ -19,7 +20,7 @@ const CellList: React.FC = () => {
   ));
 
   return (
-    <div>
+    <div className="cell-list">
       <AddCell previousCellId={null} forceVisible={cells.length === 0} />
       {renderedCells}
     </div>
