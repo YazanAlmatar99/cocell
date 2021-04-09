071f65f8532763f73b211b43f040d101c775eec5
Yazan <me@yazanalmatar.com>
2021-04-08 23:10:28 -0400

    added routing

--- src/index.tsx
+++ src/index.tsx
@@ -4,13 +4,17 @@ import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { store } from "./state";
 import CellList from "./components/cell-list";
+import Landing from './components/Landing'
+import { BrowserRouter as Router, Route } from "react-router-dom";
 // import CodeCell from "./components/code-cell";
-import TextEditor from "./components/text-editor";
 const App = () => {
   return (
     <Provider store={store}>
       <div>
-        <CellList />
+        <Router>
+          <Route exact path="/cells" component={CellList}/>
+          <Route exact path="/" component={Landing}/>
+        </Router>
       </div>
     </Provider>
   );
